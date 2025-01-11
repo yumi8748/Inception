9208f9b1dbb4f9a5b65b92aa40bccaf8ee72497c
yu-chen <yu-chen@debian.yu-chen.42.fr>
2025-01-11 10:27:11 -0600

    debugging

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-service mysql start 
+#service mysql start 
 
+mysqld_safe &
 
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
+
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';" | mysql
+
+echo "CREATE DATABASE IF NOT EXISTS $DB1_NAME ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$DB1_USER'@'%' IDENTIFIED BY '$DB1_PWD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $DB1_NAME.* TO '$DB1_USER'@'%' ;" >> db1.sql
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+#kill $(cat /var/run/mysqld/mysqld.pid)
 
-mysqld
+exec mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# create directory to use in nginx container later and also to setup the wordpress conf
-mkdir /var/www/
-mkdir /var/www/html
-
 cd /var/www/html
 
 # remove all the wordpress files if there is something from the volumes to install it again
@@ -36,7 +32,7 @@ sed -i -r "s/database_user/$DB_USER/1"  wp-config.php
 sed -i -r "s/passwod/$DB_PWD/1"    wp-config.php
 
 #line 32
-sed -i -r "s/localhost/mariadb/1"    wp-config.php  (to connect with mariadb database)
+sed -i -r "s/localhost/mariadb/1"    wp-config.php
 
 # installs WordPress and sets up the basic configuration for the site. The --url option specifies the URL of the site, --title sets the site's title, --admin_user and --admin_password set the username and password for the site's administrator account, and --admin_email sets the email address for the administrator. The --skip-email flag prevents WP-CLI from sending an email to the administrator with the login details.
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_LOGIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
@@ -51,8 +47,8 @@ wp theme install astra --activate --allow-root
 wp plugin install redis-cache --activate --allow-root
 
 
-# uses the sed command to modify the www.conf file in the /etc/php/7.3/fpm/pool.d directory. The s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g command substitutes the value 9000 for /run/php/php7.3-fpm.sock throughout the file. This changes the socket that PHP-FPM listens on from a Unix domain socket to a TCP port.
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+# uses the sed command to modify the www.conf file in the /etc/php/7.4/fpm/pool.d directory. The s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g command substitutes the value 9000 for /run/php/php7.4-fpm.sock throughout the file. This changes the socket that PHP-FPM listens on from a Unix domain socket to a TCP port.
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
 # creates the /run/php directory, which is used by PHP-FPM to store Unix domain sockets.
 mkdir /run/php
