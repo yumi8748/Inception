3b9cec3cf3caa1c8ec9baf8fd3c9a2fc674b6a11
yu-chen <yu-chen@debian.yu-chen.42.fr>
2025-01-13 07:47:20 -0600

    fixing ssl certificate setups

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -20,26 +20,26 @@ sleep 5
 
 # Set root password for MariaDB (ensure it's consistent)
 #echo "=> Setting root password..."
-#mysql -u root --execute="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB1_PWD');"
+#mysql -u root --execute="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PWD');"
 
 echo "=> Configuring root user..."
 mysql -u root --skip-password <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB1_ROOT_PWD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
 FLUSH PRIVILEGES;
 EOF
 
 # Configure database and user
 echo "=> Setting up database and user..."
 cat <<EOF > /tmp/init.sql
-CREATE DATABASE IF NOT EXISTS $DB1_NAME;
-CREATE USER IF NOT EXISTS '$DB1_USER'@'%' IDENTIFIED BY '$DB1_PWD';
-GRANT ALL PRIVILEGES ON $DB1_NAME.* TO '$DB1_USER'@'%';
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
 # Execute the initialization SQL script(using root password)
 echo "=> Executing SQL script..."
-mysql -u root -p$DB1_PWD < /tmp/init.sql
+mysql -u root -p$DB_PWD < /tmp/init.sql
 rm /tmp/init.sql
 
 echo "=> MariaDB initialization completed!"
